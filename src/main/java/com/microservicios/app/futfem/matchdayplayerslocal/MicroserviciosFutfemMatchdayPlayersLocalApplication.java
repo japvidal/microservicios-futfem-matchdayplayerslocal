2273588f0a7b689f041301b514196eb634e67a5a
japvidal <japvidal@gmail.com>
2026-03-22 18:52:15 +0100

    Initial backend import

---
+++ src/main/java/com/microservicios/app/futfem/matchdayplayerslocal/MicroserviciosFutfemMatchdayPlayersLocalApplication.java
@@ -0,0 +1,13 @@
+package com.microservicios.app.futfem.matchdayplayerslocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosFutfemMatchdayPlayersLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosFutfemMatchdayPlayersLocalApplication.class, args);
+	}
+
+}
